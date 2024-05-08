be99cc6774e8994a1508d959e64c45c2f93e8b5c
Skuddin <sheikh@nextgenitltd.com>
2024-05-09 02:01:47 +0600

    updated profile image upload

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,7 +5,7 @@
     <div class="d-flex justify-content-between">
         <div class="d-flex gap-3">
             <div class="rounded-lg">
-                <img class="rounded" id="imgSrc" alt="profile_image"/>
+                <img class="rounded" id="imgSrc" alt="profile_image" width="100"/>
             </div>
             <div>
                 <h2 class="fw-bold font-bold">{{ auth()->user()->name }}</h2>
@@ -14,25 +14,33 @@
         </div>
         <div>
             <p>Upload Profile</p>
-            <div class="upload-container">
-                <label for="image" class="file-uploader">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-upload" viewBox="0 0 16 16">
-                        <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
-                        <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708z"/>
-                    </svg>
-                    <input type="file" class="d-none" id="image"/>
-                </label>
-            </div>
+            <form id="uploadForm" enctype="multipart/form-data">
+                <div class="upload-container">
+                    <label for="image" class="file-uploader">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-upload" viewBox="0 0 16 16">
+                            <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+                            <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708z"/>
+                        </svg>
+                        <input type="file" class="d-none" id="image" name="image"/>
+                    </label>
+                </div>
+                <button type="submit">Upload</button>
+            </form>
+
         </div>
 
     </div>
 
 </div>
 
-    <script>
+@endsection
+
+@push('js')
+
+<script>
+    $(document).ready(function(){
         function loadImage() {
             let url= "{{ auth()->user()->image }}";
-
             if(url.match(/\.(jpeg|jpg|gif|png)$/) != null){
                 return url;
             }else{
@@ -41,5 +49,33 @@
         }
 
         $("#imgSrc").attr('src', loadImage())
-    </script>
-@endsection
+
+        $('#uploadForm').on('submit', function(e){
+            e.preventDefault();
+            let formData = new FormData(this);
+            let token = "{{ csrf_token() }}";
+
+            $.ajax({
+                url: "{{ route('profile.image.upload') }}",
+                method: 'POST',
+                headers: {
+                    'X-CSRF-TOKEN': token
+                },
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function(response){
+                    // Handle success
+                    console.log(response);
+                },
+                error: function(xhr, status, error){
+                    // Handle error
+                    console.error(xhr.responseText);
+                }
+            });
+        });
+    });
+
+</script>
+
+@endpush
